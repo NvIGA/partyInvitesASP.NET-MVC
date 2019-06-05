ec4e160475eac85c93e6527dbe51aadf7430ba83
NvIGA <vovatar199627v@gmail.com>
2019-06-06 01:41:52 +0300

    Version1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+using System;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            
+            return View("MyView");
+        }   
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            return View();
+        }
+
+        public ViewResult ListResponces()
+        {
+            return View(Repository.Responces.Where(r => r.WillAttend == true));
+        }
+    }
+}
